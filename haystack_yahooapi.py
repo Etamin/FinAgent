d317312599b1e714fc486ac0c39d478ea4ccafbf
laurabernardy <laura@trux-dgx01.uni.lux>
2025-05-06 11:31:50 +0200

    translating improved, printing fixed, now with logging file, clearing comments

--- haystack_yahooapi.py
+++ haystack_yahooapi.py
@@ -8,7 +8,6 @@ from langdetect import detect
 import pandas as pd
 import sys
 import sqlite3
-import requests
 import json
 import logging
 from haystack import tracing
@@ -16,81 +15,107 @@ from haystack.tracing.logging_tracer import LoggingTracer
 from haystack.core.errors import PipelineRuntimeError
 import time
 from pdf_rag.pipelines.generate_answer import run_retriever
-#from pdf_rag.pipelines.generate_answer import run_generator
 import yfinance as yf
 import ast 
 import gradio as gr
+from datetime import datetime
+from curl_cffi import requests
 
+#logging
+class DualStreamHandler:
+    def __init__(self, terminal, file):
+        self.terminal = terminal
+        self.file = file
 
-#logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
-#logging.getLogger("haystack").setLevel(logging.DEBUG)
-#tracing.tracer.is_content_tracing_enabled = True 
-#tracing.enable_tracing(LoggingTracer(tags_color_strings={"haystack.component.input": "\x1b[1;31m", "haystack.component.name": "\x1b[1;34m"}))
+    def write(self, message):
+        if message != "\n":  # Avoid empty newline handling
+            self.terminal.write(message)  # Output to terminal
+            self.file.write(message)      # Output to file
 
-#read input question from bash
-#if len(sys.argv) < 2:
-#    print("Usage: python hstranslate.py \"Your question here\"")
-#    sys.exit(1)
-#input_text = sys.argv[1]
+    def flush(self):
+        self.terminal.flush()
+        self.file.flush()
 
+    def isatty(self):
+        return self.terminal.isatty()
+    
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_file = open(f"FinAgent_log_{timestamp}.log", "w")
+logging.basicConfig(filename=f"FinAgent_log_{timestamp}.log", level=logging.DEBUG)
+log_stream = open(f"FinAgent_log_{timestamp}.log", "a")  # Open in append mode
+dual_handler = DualStreamHandler(sys.stdout, log_stream)
+sys.stdout = dual_handler
+sys.stderr = dual_handler
+tracing.tracer.is_content_tracing_enabled = True
+tracing.enable_tracing(LoggingTracer(tags_color_strings={ 
+    "haystack.component.input": "\x1b[1;31m", 
+    "haystack.component.name": "\x1b[1;34m"
+}))
 
 
 def main(input_text, _=None):
     allstarttime = time.time()
 
     def format_execution_time(start_time, end_time):
-    # Calculate the time difference
         execution_time = end_time - start_time
-    
-        # Convert to hh:mm:ss format
         hours, rem = divmod(execution_time, 3600)
         minutes, seconds = divmod(rem, 60)
-        # time_formatted = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
         time_formatted = "{}h {}m {}s".format(int(hours), int(minutes), int(seconds))
-    
         return time_formatted
+
 
     input_text = f"{input_text}"
 
     ###TRANSLATION
     start_time = time.time()
     ##translation templates
-    template_de = "Translate {{query}} to German. Just answer with the translated text." #without pivoting, comment out
+    langtemplate = "detect the language in the following sentence: {{query}}. Answer only with the ISO 639 language code."
+    template_de = "Translate {{query}} to German. Just answer with the translated text."
     template_en = "Translate {{query}} to English. Just answer with the translated text."
 
     pipe = Pipeline()
 
     ##translation components
-    pipe.add_component("prompt_builder", PromptBuilder(template=template_de, required_variables=[]))#without pivoting, comment out
-    pipe.add_component("llm", OllamaGenerator(model="gemma3:12b"))#without pivoting, comment out
-    pipe.connect("prompt_builder", "llm")#without pivoting, comment out
+    pipe.add_component("prompt_builder_base", PromptBuilder(template=langtemplate, required_variables=[]))
+    pipe.add_component("basellm", OllamaGenerator(model="gemma3:12b"))
+    pipe.connect("prompt_builder_base", "basellm")
+
+    pipe.add_component("prompt_builder", PromptBuilder(template=template_de, required_variables=[]))
+    pipe.add_component("llm", OllamaGenerator(model="gemma3:12b"))
+    pipe.connect("prompt_builder", "llm")
+
 
     pipe.add_component("prompt_builder1", PromptBuilder(template=template_en, required_variables=[]))
     pipe.add_component("llm1", OllamaGenerator(model="gemma3:12b"))
     pipe.connect("prompt_builder1", "llm1")
 
-    lang = detect(input_text) 
     ##translation output
-    #first check language. Run if it is not English. Otherwise use English output like it is. 
-    if lang == 'en':
+    #first check language
+    lang1 = pipe.run({"prompt_builder_base":{"query": input_text}})
+    lang = lang1["basellm"]["replies"][0]
+
+    if lang == 'en': #keep as it is
         question = input_text
-    else:
-        output = pipe.run({"prompt_builder":{"query": input_text}})#without pivoting, comment out
-        output1 = output["llm"]["replies"][0]#without pivoting, comment out
-        question = pipe.run({"prompt_builder1":{"query": output1}})#without pivoting, change output1 to input_text
+    elif lang == 'lb': #pivoting with german
+        output = pipe.run({"prompt_builder":{"query": input_text}})
+        output1 = output["llm"]["replies"][0]
+        question = pipe.run({"prompt_builder1":{"query": output1}})
         question = question["llm1"]["replies"][0]
+    else: #translate de and fr to en
+        output = pipe.run({"prompt_builder1":{"query": input_text}})
+        question = output["llm1"]["replies"]
+
     end_time = time.time()   
     translation_time = end_time - start_time
     translation_time = format_execution_time(start_time, end_time) 
-    print(f"Translation process finished. Total Translation time: {translation_time}")
+    print(f"\n\nTranslation process finished. Total Translation time: {translation_time}\n\n")
 
     def classify_direction(question):
         ###Decide which interface to use and output the direction
         start_time = time.time()
         routpipe = Pipeline()
 
-        # Prompt template for port (sql/api/rag) classification [INSERT]
-        #TO DO: BETTER PROMPT!!!
+        # Prompt template for port (sql/api/rag) classification
         routprompt = """
         Create a classification result from the question: {{question}}.
 
@@ -113,7 +138,7 @@ def main(input_text, _=None):
         end_time = time.time()   
         routing_time = end_time - start_time
         routing_time = format_execution_time(start_time, end_time) 
-        print(f"Routing process finished. Total Routing time: {routing_time}")
+        print(f"\n\nRouting process finished. Total Routing time: {routing_time}\n\n")
         return question, direction
 
     @component
@@ -128,7 +153,6 @@ def main(input_text, _=None):
             for query in queries:
               query=query.replace("```sql","").replace("```","")
               query=query.replace("sql","").replace("","")
-              #print(query)
               result = pd.read_sql(query, self.connection)
               results.append(f"{result}")
             self.connection.close() 
@@ -137,10 +161,8 @@ def main(input_text, _=None):
     def sqlpipe(question):
         start_time = time.time()
         ###SQL Query
-        # Define database schema, [INSERT] if we use another one
         columns = "ROWID;transaction_id;account_id;date;amount;description;type"
 
-        # Modify table name if needed [INSERT]
         sql_prompt = """Please generate an SQL query. The query should answer the following Question: {{question}};
                     The query is to be answered for the table is called 'transactions' with the following
                     Columns: {{columns}};"
@@ -156,7 +178,6 @@ def main(input_text, _=None):
         sql_pipe.connect("sql_prompt", "sqlllm")
         sql_pipe.connect("sqlllm.replies", "sql_querier.queries")
 
-    ####TO DO: No fallback to RAG
         try:
             fullresult = sql_pipe.run({
             "sql_prompt": {"question": question, "columns": columns},})
@@ -165,15 +186,15 @@ def main(input_text, _=None):
                 result = "No Answer"
                 #result = ragpipe(question) #If sql retrieves no answer, go to rag (questions to similar)
                 #fullresult = "RAG"
-        except PipelineRuntimeError as e: #if runtimeerror because no cql result, go to rag (questions to similar)
-            print(f"SQL Pipeline failed with error: {e}") 
+        except PipelineRuntimeError as e: 
+            print(f"\n\nSQL Pipeline failed with error: {e}\n\n") 
             result = "No Answer"
             #result = ragpipe(question)
             #fullresult = "RAG"
         end_time = time.time()   
         sql_time = end_time - start_time
         sql_time = format_execution_time(start_time, end_time) 
-        print(f"SQL process finished. Total SQL time: {sql_time}")
+        print(f"\n\nSQL process finished. Total SQL time: {sql_time}\n\n")
         return result, fullresult 
 
 
@@ -206,14 +227,14 @@ def main(input_text, _=None):
         llm_reply = result["apillm"]["replies"][0]
 
         try:
-            # Use ast.literal_eval to safely parse the list
             api_prompt_result = ast.literal_eval(llm_reply)
             ticker_symbol, period = api_prompt_result[0], api_prompt_result[1]
 
 
+            session = requests.Session(impersonate="chrome")
 
-            # Get stock data using yfinance
-            ticker = yf.Ticker(ticker_symbol)
+            #Get stock data using yfinance
+            ticker = yf.Ticker(ticker_symbol, session=session)
             time.sleep(1)
             data = ticker.history(period=period)
             time.sleep(1)
@@ -228,15 +249,13 @@ def main(input_text, _=None):
             end_time = time.time()   
             api_time = end_time - start_time
             api_time = format_execution_time(start_time, end_time) 
-            print(f"API process finished. Total API time: {api_time}")
+            print(f"\n\nAPI process finished. Total API time: {api_time}\n\n")
             return result, fullapiresult
 
         except Exception as e:
             result = "No Answer"
             fullapiresult = "Broken Api Call"
             return result, fullapiresult
-            #print("Error in API pipeline", e)
-
 
     def ragpipe(query):
         start_time = time.time()
@@ -254,24 +273,20 @@ def main(input_text, _=None):
         top_k = 30
         top_k_r = 5
         embedder_name = embedders_mapping['gte-base']
-        llm = 'gemma3:12b'
-        # User question
         query = question
-        # Run retriever
         result = run_retriever(query, embedder_name, top_k, top_k_r)
         full_result = f"Answer found via RAG." + f"\n"+ "No Metadata yet"
-        #result = run_generator(query, contexts, llm)
         end_time = time.time()   
         rag_time = end_time - start_time
         rag_time = format_execution_time(start_time, end_time) 
-        print(f"RAG process finished. Total RAG time: {rag_time}")
+        print(f"\n\nRAG process finished. Total RAG time: {rag_time}\n\n")
         return result, full_result
 
     def nl_answer(question, result, lang):
         ##Answer in Natural Language
         start_time = time.time()
         nllm = OllamaGenerator(model="gemma3:12b")
-        out_prompt= PromptBuilder(template="""Based on question: '{{question}}' provide the result:{{query_result}}. Build an answer in normal language containing both. Do not provide extra Information (like language used) or explanations, just the plain Answer. Explain not, what any model does or did. Provide in English and a second time in language: {{lang}}. If there is 'None', 'No Answer' or an empty string in the result, just answer 'No Answer.'""", required_variables=[])
+        out_prompt= PromptBuilder(template="""Based on question: '{{question}}' provide the result:{{query_result}}. Build an answer in normal language containing both. Do not provide extra Information (like language used) or explanations, just the plain Answer. Explain not, what any model does or did. Provide in English and a second time in the following language:{{lang}}. If there is 'None', 'No Answer' or an empty string in the result, just answer 'No Answer.'""", required_variables=[])
         answer_pipeline = Pipeline()
         answer_pipeline.add_component("out_prompt", out_prompt)
         answer_pipeline.add_component("nllm", nllm)
@@ -289,21 +304,15 @@ def main(input_text, _=None):
         end_time = time.time()   
         answer_time = end_time - start_time
         answer_time = format_execution_time(start_time, end_time) 
-        print(f"Generating Answer in Natural Language finished. Total Generation time: {answer_time}")
+        print(f"\n\nGenerating Answer in Natural Language finished. Total Generation time: {answer_time}\n\n")
         return result
 
-    #question = translate_to_english(input_text)
     question, direction = classify_direction(question)
     direction = direction.strip().lower()
 
-    ###MAIN ACTION
-
-    ###TO DO: remove RAG fallback
+    ###MAIN ACTION, 
     if "sql" in direction:
         result, fullresult = sqlpipe(question)
-        #if fullresult == "RAG":
-        #    metadat = "- NOT IMPLEMENTED YET -"
-        #else:
         metadat = fullresult['sql_querier']['queries']
         metadat=metadat[0]
         metadat=metadat.replace("```sql","").replace("```","")
@@ -322,20 +331,24 @@ def main(input_text, _=None):
     allend_time = time.time()   
     all_time = allend_time - allstarttime
     all_time = format_execution_time(allstarttime, allend_time) 
-    print(f"The whole process finished. Total process time: {all_time}")
+    print(f"\n\nThe whole process finished. Total process time: {all_time}\n\n")
+    log_file.close()
+    sys.stdout = sys.__stdout__
+    sys.stderr = sys.__stderr__
     return final_output1
 
 
 
+###UI
 theme = gr.themes.Base().set(
     body_text_color='white',
     background_fill_primary='black',
-    block_background_fill='black',
-    block_border_color='*primary_950',
+    block_background_fill='*primary_950',
+    block_border_color='*primary_900',
     block_info_text_color='white',
     block_label_background_fill='*primary_50',
     block_title_text_color='white',
-    input_background_fill='*primary_50'
+    input_background_fill='black'
 )
 def clear_inputs():
     return "", ""
